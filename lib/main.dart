b1bdf62f4c89fb2715b0acbdaa9e6aa597747885
2Math0 <toto.mm50.tm@gmail.com>
2022-11-25 19:36:58 +0200

    refactor app.dart applying new structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
-import 'package:ecommerce_flutter/presentation/resources/theme_manager.dart';
-import 'package:ecommerce_flutter/presentation/view/parent_bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'app/app.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark));
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E commerce',
-      debugShowCheckedModeBanner: false,
-      theme: getApplicationTheme(),
-      home: const MarketParent(),
-    );
-  }
 }
