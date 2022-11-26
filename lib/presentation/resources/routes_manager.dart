1d51afe6c8548e8fad13d9842a91aef4c345c81c
2Math0 <toto.mm50.tm@gmail.com>
2022-11-26 08:29:20 +0200

    adding routes manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String splashRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String marketRoute = '/main';
+  static const String favouritesRoute = '/storeDetails';
+}
