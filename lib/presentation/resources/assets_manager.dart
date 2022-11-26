4c2fcfc3314ce6ae56247e0db642acc7800fd991
2Math0 <toto.mm50.tm@gmail.com>
2022-11-26 22:27:03 +0200

    adding assets_manager.dart

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,21 @@
+const String PRODUCT_ICON_PATH = "assets/icons/Product_icon";
+
+class ProductIcon {
+  static const String bikiniProductIcon = '$PRODUCT_ICON_PATH/bikini.png';
+  static const String dressProductIcon = '$PRODUCT_ICON_PATH/dress.png';
+  static const String manBagProductIcon = '$PRODUCT_ICON_PATH/man bag.png';
+  static const String manPantsProductIcon = '$PRODUCT_ICON_PATH/man pants.png';
+  static const String manShoesProductIcon = '$PRODUCT_ICON_PATH/man shoes.png';
+  static const String manUnderwearProductIcon =
+      '$PRODUCT_ICON_PATH/man underwear.png';
+  static const String shirtProductIcon = '$PRODUCT_ICON_PATH/shirt.png';
+  static const String skirtProductIcon = '$PRODUCT_ICON_PATH/skirt.png';
+  static const String tshirtProductIcon = '$PRODUCT_ICON_PATH/Tshirt.png';
+  static const String womanBagProductIcon = '$PRODUCT_ICON_PATH/woman bag.png';
+  static const String womanPantsProductIcon =
+      '$PRODUCT_ICON_PATH/woman pants.png';
+  static const String womanShoesProductIcon =
+      '$PRODUCT_ICON_PATH/woman shoes.png';
+  static const String womanTshirtProductIcon =
+      '$PRODUCT_ICON_PATH/woman tshirt.png';
+}
